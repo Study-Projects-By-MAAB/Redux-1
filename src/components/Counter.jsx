fbfb51f31145c74632e28e05d4773c885c190949
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-02-03 22:56:41 +0600

    basic redux setup

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
+
+export default function Counter() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>
+        <h1>{count}</h1>
+      </div>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
