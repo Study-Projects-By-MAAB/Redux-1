1411ec3f5f898ef344d07ca4b25a5237129788af
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-02-04 20:20:11 +0600

    redux logger middleware implemented

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import counterReducer from "./features/counter/counterSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
